478cd8b513126f86ae6f023b4ee38963b84fd1e2
ding <31501113@stu.zucc.edu.cn>
2018-02-27 21:07:19 +0800

    一个时钟小控件

---
+++ 1-时钟/main.js
@@ -0,0 +1,22 @@
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({width: 355, height: 130,frame:false,resizable:false})
+
+  // 然后加载应用的 index.html。
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+}
+
+app.on('ready', createWindow)
